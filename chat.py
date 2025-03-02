96a4e0975fda78c7569000d2e1ad9db2b8773e50
pinehsh728 <63550120+pinehsh728@users.noreply.github.com>
2025-03-02 12:04:30 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -305,16 +305,16 @@ if st.session_state.chat_started:
     # 숨겨진 HTML 요소로 삽입
     def inject_ga():
     
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
-        index_path.write_text(new_html)
+        index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+        soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+        if not soup.find(id=GA_ID): 
+            bck_index = index_path.with_suffix('.bck')
+            if bck_index.exists():
+                shutil.copy(bck_index, index_path)  
+            else:
+                shutil.copy(index_path, bck_index)  
+            html = str(soup)
+            new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
+            index_path.write_text(new_html)
 
     inject_ga()
