01df9a79d0194304ed0f8510981970dbc339f374
pinehsh728 <63550120+pinehsh728@users.noreply.github.com>
2025-03-02 12:35:34 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -10,35 +10,6 @@ import pathlib
 import shutil
 
 
-#Google Crawling Code
-GA_ID= "G-QV3YB9WMBE"
-GA_SCRIPT ="""
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QV3YB9WMBE"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){{dataLayer.push(arguments);}}
-      gtag('js', new Date());        
-      gtag('config', 'G-QV3YB9WMBE');
-    </script>
-"""
-
-# 숨겨진 HTML 요소로 삽입
-def inject_ga():
-
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copyfile(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
-        index_path.write_text(new_html)
-
-inject_ga()
 
 
 
